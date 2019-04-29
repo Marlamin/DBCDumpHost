544a000e0d1e206c5ef2386b345b2c4cf96bdd43
Martin Benjamins <ictmartin@gmail.com>
2019-04-29 16:57:37 +0200

    Add project files

---
+++ DBCDumpHost/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DBCDumpHost.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [Route("")]
+        public ActionResult<string> Index()
+        {
+            return "Hello!";
+        }
+    }
+}
---
+++ DBCDumpHost/Controllers/ReloadDefsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DBCDumpHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReloadDefsController : ControllerBase
+    {
+        // GET: api/ReloadDefs
+        [HttpGet]
+        public string Get()
+        {
+            DefinitionManager.LoadDefinitions();
+            return "Reloaded " + DefinitionManager.definitionLookup.Count + " definitions!";
+        }
+    }
+}
