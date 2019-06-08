4ea56ad3d9d615b8aa9f5665c9dcd9fd24c8a0be
Martin Benjamins <marlamin@marlamin.com>
2019-06-09 01:23:51 +0200

    Add DBCDTest project for testing multiple DBCs in one go

---
+++ DBCDTester/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace DBCDTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbcProvider = new DBCProvider();
+            var dbdProvider = new DBDProvider();
+            if(args.Length > 0)
+            {
+                foreach (var file in Directory.GetFiles(Path.Combine(SettingManager.dbcDir, args[0], "dbfilesclient"), "*.*", SearchOption.AllDirectories))
+                {
+                    var db = Path.GetFileNameWithoutExtension(file);
+                    try
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(file.Replace(SettingManager.dbcDir + "\\", ""));
+                        DBCD.DBCD storage = new DBCD.DBCD(dbcProvider, dbdProvider);
+                        storage.Load(db, args[0]);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message + " " + e.StackTrace);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var dir in Directory.GetDirectories(SettingManager.dbcDir))
+                {
+                    var build = dir.Replace(SettingManager.dbcDir + "\\", "");
+                    foreach (var file in Directory.GetFiles(Path.Combine(dir, "dbfilesclient"), "*.*", SearchOption.AllDirectories))
+                    {
+                        var db = Path.GetFileNameWithoutExtension(file);
+                        try
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(file.Replace(SettingManager.dbcDir + "\\", ""));
+                            DBCD.DBCD storage = new DBCD.DBCD(dbcProvider, dbdProvider);
+                            storage.Load(db, build);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message + " " + e.StackTrace);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
