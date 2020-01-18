74251b654c8dd1ca87d72ec5e812397edf5b2677
Martin Benjamins <marlamin@marlamin.com>
2020-01-18 16:07:22 +0100

    Add encrypted DB2 sections information endpoint

---
+++ DBCDumpHost/Controllers/EncryptedSectionController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBCD.Providers;
+using DBCDumpHost.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBCDumpHost.Controllers
+{
+    [Route("api/sectioninfo")]
+    [ApiController]
+    public class EncryptedSectionController : ControllerBase
+    {
+        private readonly DBDProvider dbdProvider;
+        private readonly DBCManager dbcManager;
+
+        public EncryptedSectionController(IDBDProvider dbdProvider, IDBCManager dbcManager)
+        {
+            this.dbdProvider = dbdProvider as DBDProvider;
+            this.dbcManager = dbcManager as DBCManager;
+        }
+
+        [HttpGet]
+        public string Get(string name, string build, bool useHotfixes = false)
+        {
+            if(name == null || build == null)
+            {
+                return "Not enough variables";
+            }
+
+            var storage = dbcManager.GetOrLoad(name, build, useHotfixes);
+            if (!storage.Values.Any())
+            {
+                throw new Exception("No rows found!");
+            }
+
+            var returnString = "";
+            foreach(var encryptedSection in storage.GetEncryptedSections())
+            {
+                returnString += encryptedSection.Key.ToString("X16") + " " + encryptedSection.Value + "\n"; 
+            }
+
+            return returnString;
+        }
+    }
+}
