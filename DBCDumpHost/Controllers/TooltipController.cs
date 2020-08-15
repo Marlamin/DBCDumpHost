4d7c1a667cd43f1ce57ead248a945a71a8d31139
Martin Benjamins <marlamin@marlamin.com>
2020-08-16 00:58:19 +0200

    Output item stats in tooltips

--- DBCDumpHost/Controllers/TooltipController.cs
+++ DBCDumpHost/Controllers/TooltipController.cs
@@ -24,10 +24,12 @@ namespace DBCDumpHost.Controllers
         public byte OverallQualityID { get; set; } 
         public bool HasSparse { get; set; }
         public TTItemEffect[] ItemEffects { get; set; }
+        public TTItemStat[] Stats { get; set; }
         public string Speed { get; set; }
         public string DPS { get; set; }
         public string MinDamage { get; set; }
         public string MaxDamage { get; set; }
+        public sbyte RequiredLevel { get; set; }
     }
 
     struct TTItemEffect
@@ -41,6 +43,12 @@ namespace DBCDumpHost.Controllers
         public int SpellID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+    }
+
+    struct TTItemStat
+    {
+        public sbyte StatTypeID { get; set; }
+        public double Value { get; set; }
     }
 
     enum InventoryType : sbyte
@@ -74,6 +82,70 @@ namespace DBCDumpHost.Controllers
         RangedRight = 26,
         Quiver = 27,
         Relic = 28
+    }
+
+    enum ItemStatType : sbyte
+    {
+        MANA = 0,
+        HEALTH = 1,
+        AGILITY = 3,
+        STRENGTH = 4,
+        INTELLECT = 5,
+        SPIRIT = 6,
+        STAMINA = 7,
+        DEFENSE_SKILL_RATING = 12,
+        DODGE_RATING = 13,
+        PARRY_RATING = 14,
+        BLOCK_RATING = 15,
+        HIT_MELEE_RATING = 16,
+        HIT_RANGED_RATING = 17,
+        HIT_SPELL_RATING = 18,
+        CRIT_MELEE_RATING = 19,
+        CRIT_RANGED_RATING = 20,
+        CRIT_SPELL_RATING = 21,
+        CORRUPTION = 22,
+        CORRUPTION_RESISTANCE = 23,
+        MODIFIED_CRAFTING_STAT_1 = 24,
+        MODIFIED_CRAFTING_STAT_2 = 25,
+        CRIT_TAKEN_RANGED_RATING = 26,
+        CRIT_TAKEN_SPELL_RATING = 27,
+        HASTE_MELEE_RATING = 28,
+        HASTE_RANGED_RATING = 29,
+        HASTE_SPELL_RATING = 30,
+        HIT_RATING = 31,
+        CRIT_RATING = 32,
+        HIT_TAKEN_RATING = 33,
+        CRIT_TAKEN_RATING = 34,
+        RESILIENCE_RATING = 35,
+        HASTE_RATING = 36,
+        EXPERTISE_RATING = 37,
+        ATTACK_POWER = 38,
+        RANGED_ATTACK_POWER = 39,
+        VERSATILITY = 40,
+        SPELL_HEALING_DONE = 41,
+        SPELL_DAMAGE_DONE = 42,
+        MANA_REGENERATION = 43,
+        ARMOR_PENETRATION_RATING = 44,
+        SPELL_POWER = 45,
+        HEALTH_REGEN = 46,
+        SPELL_PENETRATION = 47,
+        BLOCK_VALUE = 48,
+        MASTERY_RATING = 49,
+        EXTRA_ARMOR = 50,
+        FIRE_RESISTANCE = 51,
+        FROST_RESISTANCE = 52,
+        HOLY_RESISTANCE = 53,
+        SHADOW_RESISTANCE = 54,
+        NATURE_RESISTANCE = 55,
+        ARCANE_RESISTANCE = 56,
+        PVP_POWER = 57,
+        CR_AMPLIFY = 58,
+        CR_MULTISTRIKE = 59,
+        CR_READINESS = 60,
+        CR_SPEED = 61,
+        CR_LIFESTEAL = 62,
+        CR_AVOIDANCE = 63,
+        CR_STURDINESS = 64
     }
 
     [Route("api/tooltip")]
@@ -149,6 +221,9 @@ namespace DBCDumpHost.Controllers
                 else
                 {
                     result.Name = (string)itemSearchNameEntry["Display_lang"];
+                    result.RequiredLevel = (sbyte)itemSearchNameEntry["RequiredLevel"];
+                    result.ExpansionID = (byte)itemSearchNameEntry["ExpansionID"];
+                    result.ItemLevel = (ushort)itemSearchNameEntry["ItemLevel"];
                 }
 
                 result.HasSparse = false;
@@ -165,9 +240,64 @@ namespace DBCDumpHost.Controllers
                 result.ItemLevel = (ushort)itemSparseEntry["ItemLevel"];
                 result.OverallQualityID = (byte)itemSparseEntry["OverallQualityID"];
 
-                // WIP - Testing
-                var crRow = GameTableProvider.GetCombatRatingsMultByILVLRow(result.ItemLevel, build);
-                var randProp = await GetRandomPropertyByInventoryType(result.ItemLevel, result.OverallQualityID, (InventoryType)result.InventoryType, result.SubClassID, build);
+                var statTypes = itemSparseEntry.FieldAs<sbyte[]>("StatModifier_bonusStat");
+                if(statTypes.Length > 0 && !statTypes.All(x => x == -1) && !statTypes.All(x => x == 0))
+                {
+                    var randProp = await GetRandomPropertyByInventoryType(result.ItemLevel, result.OverallQualityID, (InventoryType)result.InventoryType, result.SubClassID, build);
+                    
+                    var statPercentEditor = itemSparseEntry.FieldAs<int[]>("StatPercentEditor");
+
+                    var statList = new Dictionary<sbyte, TTItemStat>();
+                    for (var statIndex = 0; statIndex < statTypes.Length; statIndex++)
+                    {
+                        if (statTypes[statIndex] == -1 || statTypes[statIndex] == 0)
+                            continue;
+
+
+                        var multiplierRow = new GameTableProvider.MultByILVLRow(){
+                            ArmorMultiplier = 1.0d,
+                            JewelryMultiplier = 1.0d,
+                            TrinketMultiplier = 1.0d,
+                            WeaponMultiplier = 1.0d
+                        };
+
+                        if (IsCombatRating(statTypes[statIndex]))
+                        {
+                            multiplierRow = GameTableProvider.GetCombatRatingsMultByILVLRow(result.ItemLevel, build);
+                        }
+                        else if((ItemStatType)statTypes[statIndex] == ItemStatType.STAMINA)
+                        {
+                            multiplierRow = GameTableProvider.GetStaminaMultByILVLRow(result.ItemLevel, build);
+                        }
+
+
+                        double calculatedValue;
+
+                        if ((InventoryType)result.InventoryType == InventoryType.Neck || (InventoryType)result.InventoryType == InventoryType.Finger)
+                        {
+                            calculatedValue = randProp * statPercentEditor[statIndex] * multiplierRow.JewelryMultiplier * 0.0001;
+                        }
+                        else
+                        {
+                            calculatedValue = randProp * statPercentEditor[statIndex] * multiplierRow.ArmorMultiplier * 0.0001;
+                        }
+
+                        if (statList.TryGetValue(statTypes[statIndex], out var currStat))
+                        {
+                            currStat.Value += calculatedValue;
+                        }
+                        else
+                        {
+                            statList.Add(statTypes[statIndex], new TTItemStat()
+                            {
+                                StatTypeID = statTypes[statIndex],
+                                Value = calculatedValue
+                            });
+                        }
+                    }
+
+                    result.Stats = statList.Values.ToArray();
+                }
 
                 var damageRecord = await FindRecords(targetDamageDB, build, "ItemLevel", result.ItemLevel);
                 var itemDamage = damageRecord[0].FieldAs<float[]>("Quality")[result.OverallQualityID];
@@ -176,6 +306,8 @@ namespace DBCDumpHost.Controllers
                 result.HasSparse = true;
                 result.Name = (string)itemSparseEntry["Display_lang"];
                 result.ExpansionID = (byte)itemSparseEntry["ExpansionID"];
+                result.RequiredLevel = (sbyte)itemSparseEntry["RequiredLevel"];
+
                 result.Speed = itemDelay.ToString("F2", nfi);
                 result.DPS = itemDamage.ToString("F2", nfi);
                 result.MinDamage = Math.Floor(itemDamage * itemDelay * (1 - dmgVariance * 0.5)).ToString();
@@ -273,8 +405,49 @@ namespace DBCDumpHost.Controllers
             }
         }
 
+        private bool IsCombatRating(sbyte StatTypeID)
+        {
+            switch ((ItemStatType)StatTypeID)
+            {
+                case ItemStatType.MASTERY_RATING:
+                case ItemStatType.DODGE_RATING:
+                case ItemStatType.PARRY_RATING:
+                case ItemStatType.BLOCK_RATING:
+                case ItemStatType.HIT_MELEE_RATING:
+                case ItemStatType.HIT_RANGED_RATING:
+                case ItemStatType.HIT_SPELL_RATING:
+                case ItemStatType.CRIT_MELEE_RATING:
+                case ItemStatType.CRIT_RANGED_RATING:
+                case ItemStatType.CRIT_SPELL_RATING:
+                case ItemStatType.CRIT_TAKEN_RANGED_RATING:
+                case ItemStatType.CRIT_TAKEN_SPELL_RATING:
+                case ItemStatType.HASTE_MELEE_RATING:
+                case ItemStatType.HASTE_RANGED_RATING:
+                case ItemStatType.HASTE_SPELL_RATING:
+                case ItemStatType.HIT_RATING:
+                case ItemStatType.CRIT_RATING:
+                case ItemStatType.HIT_TAKEN_RATING:
+                case ItemStatType.CRIT_TAKEN_RATING:
+                case ItemStatType.RESILIENCE_RATING:
+                case ItemStatType.HASTE_RATING:
+                case ItemStatType.EXPERTISE_RATING:
+                case ItemStatType.CR_MULTISTRIKE:
+                case ItemStatType.CR_SPEED:
+                case ItemStatType.CR_LIFESTEAL:
+                case ItemStatType.CR_AVOIDANCE:
+                case ItemStatType.VERSATILITY:
+                case ItemStatType.EXTRA_ARMOR:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
         private async Task<int> GetRandomPropertyByInventoryType(ushort itemLevel, byte overallQualityID, InventoryType inventoryType, byte subClassID, string build)
         {
+            if (overallQualityID == 0)
+                return 0;
+
             sbyte targetIndex = -1;
             switch (inventoryType)
             {
@@ -288,18 +461,18 @@ namespace DBCDumpHost.Controllers
                 case InventoryType.Thrown:
                     targetIndex = 0;
                     break;
-                case InventoryType.Neck:
-                case InventoryType.Wrist:
-                case InventoryType.Finger:
-                case InventoryType.Back:
-                    targetIndex = 2;
-                    break;
                 case InventoryType.Shoulder:
                 case InventoryType.Waist:
                 case InventoryType.Feet:
                 case InventoryType.Hands:
                 case InventoryType.Trinket:
                     targetIndex = 1;
+                    break;
+                case InventoryType.Neck:
+                case InventoryType.Wrist:
+                case InventoryType.Finger:
+                case InventoryType.Back:
+                    targetIndex = 2;
                     break;
                 case InventoryType.OneHand:
                 case InventoryType.Shield:
@@ -321,22 +494,13 @@ namespace DBCDumpHost.Controllers
             var randomPropDB = await dbcManager.GetOrLoad("RandPropPoints", build);
             if (randomPropDB.TryGetValue(itemLevel, out DBCDRow randPropEntry))
             {
-                var targetField = "";
-                switch (overallQualityID)
+                string targetField = overallQualityID switch
                 {
-                    case 2:
-                        targetField = "Good";
-                        break;
-                    case 3:
-                        targetField = "Superior";
-                        break;
-                    case 4:
-                        targetField = "Epic";
-                        break;
-                    default:
-                        throw new Exception("Unsupported quality: " + overallQualityID);
-                }
-
+                    2 => "Good",
+                    3 => "Superior",
+                    4 => "Epic",
+                    _ => throw new Exception("Unsupported quality: " + overallQualityID),
+                };
                 return (int)randPropEntry.FieldAs<uint[]>(targetField)[targetIndex];
             }
             else
--- DBCDumpHost/Services/GameTableProvider.cs
+++ DBCDumpHost/Services/GameTableProvider.cs
@@ -9,9 +9,10 @@ namespace DBCDumpHost.Services
 {
     public static class GameTableProvider
     {
-        private static Dictionary<string, Dictionary<int, CombatRatingsMultByILVLRow>> combatRatingMultiByILVL;
+        private static Dictionary<string, Dictionary<int, MultByILVLRow>> combatRatingMultiByILVL;
+        private static Dictionary<string, Dictionary<int, MultByILVLRow>> staminaMultiByILVL;
 
-        public struct CombatRatingsMultByILVLRow
+        public struct MultByILVLRow
         {
             public double ArmorMultiplier;
             public double WeaponMultiplier;
@@ -21,16 +22,62 @@ namespace DBCDumpHost.Services
 
         static GameTableProvider()
         {
-            combatRatingMultiByILVL = new Dictionary<string, Dictionary<int, CombatRatingsMultByILVLRow>>();
+            combatRatingMultiByILVL = new Dictionary<string, Dictionary<int, MultByILVLRow>>();
+            staminaMultiByILVL = new Dictionary<string, Dictionary<int, MultByILVLRow>>();
         }
 
-        public static CombatRatingsMultByILVLRow GetCombatRatingsMultByILVLRow(int itemLevel, string build)
+        public static MultByILVLRow GetStaminaMultByILVLRow(int itemLevel, string build)
+        {
+            if (!staminaMultiByILVL.ContainsKey(build))
+            {
+                using (var client = new HttpClient())
+                {
+                    var tempDict = new Dictionary<int, MultByILVLRow>();
+                    var output = client.GetStringAsync(SettingManager.cascToolHost + "/casc/file/gametable?gameTableName=staminamultbyilvl&fullBuild=" + build).Result;
+                    var lines = output.Split("\r\n");
+                    for (var i = 1; i < lines.Length; i++)
+                    {
+                        if (lines[i].Length == 0)
+                            continue;
+
+                        var fields = lines[i].Split('\t');
+                        tempDict.Add(int.Parse(fields[0]),
+                            new MultByILVLRow()
+                            {
+                                ArmorMultiplier = double.Parse(fields[1], CultureInfo.InvariantCulture),
+                                WeaponMultiplier = double.Parse(fields[2], CultureInfo.InvariantCulture),
+                                TrinketMultiplier = double.Parse(fields[3], CultureInfo.InvariantCulture),
+                                JewelryMultiplier = double.Parse(fields[4], CultureInfo.InvariantCulture)
+                            });
+                    }
+                    staminaMultiByILVL.Add(build, tempDict);
+                }
+            }
+
+            if (staminaMultiByILVL.TryGetValue(build, out var buildDict))
+            {
+                if (buildDict.TryGetValue(itemLevel, out var row))
+                {
+                    return row;
+                }
+                else
+                {
+                    throw new Exception("Target itemLevel not found in gametable!");
+                }
+            }
+            else
+            {
+                throw new Exception("Target build not found in gametable cache!");
+            }
+        }
+
+        public static MultByILVLRow GetCombatRatingsMultByILVLRow(int itemLevel, string build)
         {
             if(!combatRatingMultiByILVL.ContainsKey(build))
             {
                 using (var client = new HttpClient())
                 {
-                    var tempDict = new Dictionary<int, CombatRatingsMultByILVLRow>();
+                    var tempDict = new Dictionary<int, MultByILVLRow>();
                     var output = client.GetStringAsync(SettingManager.cascToolHost + "/casc/file/gametable?gameTableName=CombatRatingsMultByILvl&fullBuild=" + build).Result;
                     var lines = output.Split("\r\n");
                     for(var i = 1; i < lines.Length; i++)
@@ -40,7 +87,7 @@ namespace DBCDumpHost.Services
 
                         var fields = lines[i].Split('\t');
                         tempDict.Add(int.Parse(fields[0]), 
-                            new CombatRatingsMultByILVLRow() { 
+                            new MultByILVLRow() { 
                                 ArmorMultiplier = double.Parse(fields[1], CultureInfo.InvariantCulture), 
                                 WeaponMultiplier = double.Parse(fields[2], CultureInfo.InvariantCulture), 
                                 TrinketMultiplier = double.Parse(fields[3], CultureInfo.InvariantCulture), 
@@ -67,6 +114,5 @@ namespace DBCDumpHost.Services
                 throw new Exception("Target build not found in gametable cache!");
             }
         }
-
     }
 }
