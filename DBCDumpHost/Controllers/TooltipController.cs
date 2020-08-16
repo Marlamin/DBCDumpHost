695aa06c4f2fed56ba92747b6f3152e928740e50
Martin Benjamins <marlamin@marlamin.com>
2020-08-16 16:39:14 +0200

    Split up some of the tooltip logic into separate files

--- DBCDumpHost/Controllers/TooltipController.cs
+++ DBCDumpHost/Controllers/TooltipController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DBCDumpHost.Controllers
 {
-    struct TTItem
+    public struct TTItem
     {
         public string Name { get; set; }
         public int IconFileDataID { get; set; }
@@ -21,8 +21,9 @@ namespace DBCDumpHost.Controllers
         public byte SubClassID { get; set; }
         public sbyte InventoryType { get; set; }
         public ushort ItemLevel { get; set; }
-        public byte OverallQualityID { get; set; } 
+        public byte OverallQualityID { get; set; }
         public bool HasSparse { get; set; }
+        public string FlavorText { get; set; }
         public TTItemEffect[] ItemEffects { get; set; }
         public TTItemStat[] Stats { get; set; }
         public string Speed { get; set; }
@@ -32,121 +33,24 @@ namespace DBCDumpHost.Controllers
         public sbyte RequiredLevel { get; set; }
     }
 
-    struct TTItemEffect
+    public struct TTItemEffect
     {
-        public TTSpell Spell { get; set; } 
+        public TTSpell Spell { get; set; }
         public sbyte TriggerType { get; set; }
     }
 
-    struct TTSpell
+    public struct TTSpell
     {
         public int SpellID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
 
-    struct TTItemStat
+    public struct TTItemStat
     {
         public sbyte StatTypeID { get; set; }
         public int Value { get; set; }
         public bool IsCombatRating { get; set; }
-    }
-
-    enum InventoryType : sbyte
-    {
-        NonEquippable = 0,
-        Head = 1,
-        Neck = 2,
-        Shoulder = 3,
-        Shirt = 4,
-        Chest = 5,
-        Waist = 6,
-        Legs = 7,
-        Feet = 8,
-        Wrist = 9,
-        Hands = 10,
-        Finger = 11,
-        Trinket = 12,
-        OneHand = 13,
-        Shield = 14,
-        Ranged = 15,
-        Back = 16,
-        TwoHand = 17,
-        Bag = 18,
-        Tabard = 19,
-        Robe = 20,
-        MainHand = 21,
-        OffHand = 22,
-        HeldInOffhand = 23,
-        Ammo = 24,
-        Thrown = 25,
-        RangedRight = 26,
-        Quiver = 27,
-        Relic = 28
-    }
-
-    enum ItemStatType : sbyte
-    {
-        MANA = 0,
-        HEALTH = 1,
-        AGILITY = 3,
-        STRENGTH = 4,
-        INTELLECT = 5,
-        SPIRIT = 6,
-        STAMINA = 7,
-        DEFENSE_SKILL_RATING = 12,
-        DODGE_RATING = 13,
-        PARRY_RATING = 14,
-        BLOCK_RATING = 15,
-        HIT_MELEE_RATING = 16,
-        HIT_RANGED_RATING = 17,
-        HIT_SPELL_RATING = 18,
-        CRIT_MELEE_RATING = 19,
-        CRIT_RANGED_RATING = 20,
-        CRIT_SPELL_RATING = 21,
-        CORRUPTION = 22,
-        CORRUPTION_RESISTANCE = 23,
-        MODIFIED_CRAFTING_STAT_1 = 24,
-        MODIFIED_CRAFTING_STAT_2 = 25,
-        CRIT_TAKEN_RANGED_RATING = 26,
-        CRIT_TAKEN_SPELL_RATING = 27,
-        HASTE_MELEE_RATING = 28,
-        HASTE_RANGED_RATING = 29,
-        HASTE_SPELL_RATING = 30,
-        HIT_RATING = 31,
-        CRIT_RATING = 32,
-        HIT_TAKEN_RATING = 33,
-        CRIT_TAKEN_RATING = 34,
-        RESILIENCE_RATING = 35,
-        HASTE_RATING = 36,
-        EXPERTISE_RATING = 37,
-        ATTACK_POWER = 38,
-        RANGED_ATTACK_POWER = 39,
-        VERSATILITY = 40,
-        SPELL_HEALING_DONE = 41,
-        SPELL_DAMAGE_DONE = 42,
-        MANA_REGENERATION = 43,
-        ARMOR_PENETRATION_RATING = 44,
-        SPELL_POWER = 45,
-        HEALTH_REGEN = 46,
-        SPELL_PENETRATION = 47,
-        BLOCK_VALUE = 48,
-        MASTERY_RATING = 49,
-        EXTRA_ARMOR = 50,
-        FIRE_RESISTANCE = 51,
-        FROST_RESISTANCE = 52,
-        HOLY_RESISTANCE = 53,
-        SHADOW_RESISTANCE = 54,
-        NATURE_RESISTANCE = 55,
-        ARCANE_RESISTANCE = 56,
-        PVP_POWER = 57,
-        CR_AMPLIFY = 58,
-        CR_MULTISTRIKE = 59,
-        CR_READINESS = 60,
-        CR_SPEED = 61,
-        CR_LIFESTEAL = 62,
-        CR_AVOIDANCE = 63,
-        CR_STURDINESS = 64
     }
 
     [Route("api/tooltip")]
@@ -171,9 +75,9 @@ namespace DBCDumpHost.Controllers
             var itemSparseDB = await dbcManager.GetOrLoad("ItemSparse", build, true);
             var itemSearchNameDB = await dbcManager.GetOrLoad("ItemSearchName", build, true);
             var unkItems = new List<int>();
-            foreach(var itemID in itemDB.Keys)
+            foreach (var itemID in itemDB.Keys)
             {
-                if(!itemSparseDB.ContainsKey(itemID) && !itemSearchNameDB.ContainsKey(itemID))
+                if (!itemSparseDB.ContainsKey(itemID) && !itemSearchNameDB.ContainsKey(itemID))
                 {
                     unkItems.Add(itemID);
                 }
@@ -198,14 +102,15 @@ namespace DBCDumpHost.Controllers
             result.SubClassID = (byte)itemEntry["SubclassID"];
             result.InventoryType = (sbyte)itemEntry["InventoryType"];
 
-            if(result.IconFileDataID == 0)
+            if (result.IconFileDataID == 0)
             {
                 // Look in ItemModifiedAppearance => ItemAppearance for proper icon
                 var itemModifiedAppearances = await FindRecords("ItemModifiedAppearance", build, "ItemID", itemID);
-                if(itemModifiedAppearances.Count > 0)
+                if (itemModifiedAppearances.Count > 0)
                 {
                     var itemAppearanceDB = await dbcManager.GetOrLoad("ItemAppearance", build);
-                    if(itemAppearanceDB.TryGetValue((ushort)itemModifiedAppearances[0]["ItemAppearanceID"], out DBCDRow itemAppearanceRow)){
+                    if (itemAppearanceDB.TryGetValue((ushort)itemModifiedAppearances[0]["ItemAppearanceID"], out DBCDRow itemAppearanceRow))
+                    {
                         result.IconFileDataID = (int)itemAppearanceRow["DefaultIconFileDataID"];
                     }
                 }
@@ -242,10 +147,21 @@ namespace DBCDumpHost.Controllers
                 result.OverallQualityID = (byte)itemSparseEntry["OverallQualityID"];
 
                 var statTypes = itemSparseEntry.FieldAs<sbyte[]>("StatModifier_bonusStat");
-                if(statTypes.Length > 0 && !statTypes.All(x => x == -1) && !statTypes.All(x => x == 0))
+                if (statTypes.Length > 0 && !statTypes.All(x => x == -1) && !statTypes.All(x => x == 0))
                 {
-                    var randProp = await GetRandomPropertyByInventoryType(result.ItemLevel, result.OverallQualityID, (InventoryType)result.InventoryType, result.SubClassID, build);
-                    
+                    var (RandomPropField, RandomPropIndex) = TooltipUtils.GetRandomPropertyByInventoryType(result.OverallQualityID, result.InventoryType, result.SubClassID, build);
+
+                    var randomPropDB = await dbcManager.GetOrLoad("RandPropPoints", build);
+                    int randProp;
+                    if (randomPropDB.TryGetValue(result.ItemLevel, out DBCDRow randPropEntry))
+                    {
+                        randProp = (int)randPropEntry.FieldAs<uint[]>(RandomPropField)[RandomPropIndex];
+                    }
+                    else
+                    {
+                        throw new Exception("Item Level " + result.ItemLevel + " not found in RandPropPoints");
+                    }
+
                     var statPercentEditor = itemSparseEntry.FieldAs<int[]>("StatPercentEditor");
 
                     var statList = new Dictionary<sbyte, TTItemStat>();
@@ -254,70 +170,18 @@ namespace DBCDumpHost.Controllers
                         if (statTypes[statIndex] == -1 || statTypes[statIndex] == 0)
                             continue;
 
-                        // TODO: Socket penalty
-                        var socketPenalty = 0.0f;
-                        var baseStat = (int)((statPercentEditor[statIndex] * randProp) * 0.000099999997f - socketPenalty + 0.5f);
+                        var stat = TooltipUtils.CalculateItemStat(statTypes[statIndex], randProp, result.ItemLevel, statPercentEditor[statIndex], 0.0f, result.OverallQualityID, result.InventoryType, result.SubClassID, build);
 
-                        var multiplierRow = new GameTableProvider.MultByILVLRow(){
-                            JewelryMultiplier = 1.0d,
-                            TrinketMultiplier = 1.0d,
-                            WeaponMultiplier = 1.0d,
-                            ArmorMultiplier = 1.0d,
-                        };
-
-                        if (IsCombatRating(statTypes[statIndex]))
-                        {
-                            multiplierRow = GameTableProvider.GetCombatRatingsMultByILVLRow(result.ItemLevel - 1, build);
-                        }
-                        else if((ItemStatType)statTypes[statIndex] == ItemStatType.STAMINA)
-                        {
-                            multiplierRow = GameTableProvider.GetStaminaMultByILVLRow(result.ItemLevel - 1, build);
-                        }
-
-                        // RandProp * StatPctEditor * StaminaMult * 0.0001 
-                        //       60 *          7889 * 1.110734158 * 0.0001
-                        double multiplier;
-                        switch ((InventoryType)result.InventoryType)
-                        {
-                            case InventoryType.Neck:
-                            case InventoryType.Finger:
-                                multiplier = multiplierRow.JewelryMultiplier;
-                                break;
-                            case InventoryType.Trinket:
-                                multiplier = multiplierRow.TrinketMultiplier;
-                                break;
-                            case InventoryType.OneHand:
-                            case InventoryType.Shield:
-                            case InventoryType.Ranged:
-                            case InventoryType.TwoHand:
-                            case InventoryType.MainHand:
-                            case InventoryType.OffHand:
-                            case InventoryType.HeldInOffhand:
-                            case InventoryType.RangedRight:
-                                multiplier = multiplierRow.WeaponMultiplier;
-                                break;
-                            default:
-                                multiplier = multiplierRow.ArmorMultiplier;
-                                break;
-                        }
-
-                        int calculatedValue = (int)(multiplier * baseStat);
-
-                        if (calculatedValue == 0)
+                        if (stat.Value == 0)
                             continue;
 
                         if (statList.TryGetValue(statTypes[statIndex], out var currStat))
                         {
-                            currStat.Value += calculatedValue;
+                            currStat.Value += stat.Value;
                         }
                         else
                         {
-                            statList.Add(statTypes[statIndex], new TTItemStat()
-                            {
-                                StatTypeID = statTypes[statIndex],
-                                Value = calculatedValue,
-                                IsCombatRating = IsCombatRating(statTypes[statIndex])
-                            });
+                            statList.Add(statTypes[statIndex], stat);
                         }
                     }
 
@@ -330,6 +194,7 @@ namespace DBCDumpHost.Controllers
 
                 result.HasSparse = true;
                 result.Name = (string)itemSparseEntry["Display_lang"];
+                result.FlavorText = (string)itemSparseEntry["Description_lang"];
                 result.ExpansionID = (byte)itemSparseEntry["ExpansionID"];
                 result.RequiredLevel = (sbyte)itemSparseEntry["RequiredLevel"];
 
@@ -340,7 +205,7 @@ namespace DBCDumpHost.Controllers
             }
 
             var itemEffectEntries = await FindRecords("ItemEffect", build, "ParentItemID", itemID);
-            if(itemEffectEntries.Count > 0)
+            if (itemEffectEntries.Count > 0)
             {
                 var spellDB = await dbcManager.GetOrLoad("Spell", build);
                 var spellNameDB = await dbcManager.GetOrLoad("SpellName", build);
@@ -360,7 +225,7 @@ namespace DBCDumpHost.Controllers
                         }
                     }
 
-                    if(spellNameDB.TryGetValue((int)itemEffectEntries[i]["SpellID"], out DBCDRow spellNameRow))
+                    if (spellNameDB.TryGetValue((int)itemEffectEntries[i]["SpellID"], out DBCDRow spellNameRow))
                     {
                         var spellName = (string)spellNameRow["Name_lang"];
                         if (!string.IsNullOrWhiteSpace(spellName))
@@ -430,101 +295,6 @@ namespace DBCDumpHost.Controllers
             }
         }
 
-        private bool IsCombatRating(sbyte StatTypeID)
-        {
-            switch ((ItemStatType)StatTypeID)
-            {
-                case ItemStatType.DODGE_RATING:
-                case ItemStatType.PARRY_RATING:
-                case ItemStatType.BLOCK_RATING:
-                case ItemStatType.HIT_MELEE_RATING:
-                case ItemStatType.HIT_RANGED_RATING:
-                case ItemStatType.HIT_SPELL_RATING:
-                case ItemStatType.CRIT_MELEE_RATING:
-                case ItemStatType.CRIT_RANGED_RATING:
-                case ItemStatType.HIT_RATING:
-                case ItemStatType.CRIT_RATING:
-                case ItemStatType.RESILIENCE_RATING:
-                case ItemStatType.HASTE_RATING:
-                case ItemStatType.EXPERTISE_RATING:
-                case ItemStatType.VERSATILITY:
-                case ItemStatType.MASTERY_RATING:
-                case ItemStatType.CR_MULTISTRIKE:
-                case ItemStatType.CR_SPEED:
-                case ItemStatType.CR_LIFESTEAL:
-                case ItemStatType.CR_AVOIDANCE:
-                case ItemStatType.CR_STURDINESS:
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        private async Task<int> GetRandomPropertyByInventoryType(ushort itemLevel, byte overallQualityID, InventoryType inventoryType, byte subClassID, string build)
-        {
-            if (overallQualityID == 0)
-                return 0;
-
-            sbyte targetIndex = -1;
-            switch (inventoryType)
-            {
-                case InventoryType.Head:
-                case InventoryType.Shirt:
-                case InventoryType.Chest:
-                case InventoryType.Legs:
-                case InventoryType.Ranged:
-                case InventoryType.TwoHand:
-                case InventoryType.Robe:
-                case InventoryType.Thrown:
-                    targetIndex = 0;
-                    break;
-                case InventoryType.Shoulder:
-                case InventoryType.Waist:
-                case InventoryType.Feet:
-                case InventoryType.Hands:
-                case InventoryType.Trinket:
-                    targetIndex = 1;
-                    break;
-                case InventoryType.Neck:
-                case InventoryType.Wrist:
-                case InventoryType.Finger:
-                case InventoryType.Back:
-                    targetIndex = 2;
-                    break;
-                case InventoryType.OneHand:
-                case InventoryType.Shield:
-                case InventoryType.MainHand:
-                case InventoryType.OffHand:
-                case InventoryType.HeldInOffhand:
-                    targetIndex = 3;
-                    break;
-                case InventoryType.RangedRight:
-                    targetIndex = 3;
-                    if(subClassID != 19) // Wands
-                        targetIndex = 0;
-                    break;
-                case InventoryType.Relic:
-                    targetIndex = 4;
-                    break;
-            }
-                    
-            var randomPropDB = await dbcManager.GetOrLoad("RandPropPoints", build);
-            if (randomPropDB.TryGetValue(itemLevel, out DBCDRow randPropEntry))
-            {
-                string targetField = overallQualityID switch
-                {
-                    2 => "Good",
-                    3 => "Superior",
-                    4 => "Epic",
-                    _ => throw new Exception("Unsupported quality: " + overallQualityID),
-                };
-                return (int)randPropEntry.FieldAs<uint[]>(targetField)[targetIndex];
-            }
-            else
-            {
-                throw new Exception("Item Level " + itemLevel + " not found in RandPropPoints");
-            }
-        }
 
         [HttpGet("spell/{SpellID}")]
         public string GetSpellTooltip(int spellID)
