7651bc54d24463c2a007211288b4752e159dd1d9
Martin Benjamins <marlamin@marlamin.com>
2020-09-27 02:13:51 +0200

    Add /find endpoints for filtering multiple DB2 records by a certain field

---
+++ DBCDumpHost/Controllers/FindController.cs
@@ -0,0 +1,129 @@
+﻿using DBCD;
+using DBCDumpHost.Services;
+using DBCDumpHost.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBCDumpHost.Controllers
+{
+    [Route("api/find")]
+    [ApiController]
+    public class FindController : ControllerBase
+    {
+        private readonly DBCManager dbcManager;
+
+        public FindController(IDBCManager dbcManager)
+        {
+            this.dbcManager = dbcManager as DBCManager;
+        }
+
+        // GET: find/
+        [HttpGet]
+        public string Get()
+        {
+            return "No DBC selected!";
+        }
+
+        // GET: find/name
+        [HttpGet("{name}")]
+        public async Task<List<Dictionary<string, string>>> Get(string name, string build, string col, int val, bool useHotfixes = false, bool calcOffset = true)
+        {
+            Logger.WriteLine("Serving foreign key row for " + name + "::" + col + " (" + build + ", hotfixes: " + useHotfixes + ") value " + val);
+
+            var storage = await dbcManager.GetOrLoad(name, build, useHotfixes);
+
+            var result = new List<Dictionary<string, string>>();
+
+            if (!storage.Values.Any())
+            {
+                return result;
+            }
+
+            if (!calcOffset && col == "ID")
+            {
+                if (storage.TryGetValue(val, out DBCDRow row))
+                {
+                    for (var i = 0; i < storage.AvailableColumns.Length; ++i)
+                    {
+                        string fieldName = storage.AvailableColumns[i];
+
+                        if (fieldName != col)
+                            continue;
+
+                        var field = row[fieldName];
+
+                        // Don't think FKs to arrays are possible, so only check regular value
+                        if (field.ToString() == val.ToString())
+                        {
+                            var newDict = new Dictionary<string, string>();
+                            for (var j = 0; j < storage.AvailableColumns.Length; ++j)
+                            {
+                                string subfieldName = storage.AvailableColumns[j];
+                                var subfield = row[subfieldName];
+
+                                if (subfield is Array a)
+                                {
+                                    for (var k = 0; k < a.Length; k++)
+                                    {
+                                        newDict.Add(subfieldName + "[" + k + "]", a.GetValue(k).ToString());
+                                    }
+                                }
+                                else
+                                {
+                                    newDict.Add(subfieldName, subfield.ToString());
+                                }
+                            }
+
+                            result.Add(newDict);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (DBCDRow row in storage.Values)
+                {
+                    for (var i = 0; i < storage.AvailableColumns.Length; ++i)
+                    {
+                        string fieldName = storage.AvailableColumns[i];
+
+                        if (fieldName != col)
+                            continue;
+
+                        var field = row[fieldName];
+
+                        // Don't think FKs to arrays are possible, so only check regular value
+                        if (field.ToString() == val.ToString())
+                        {
+                            var newDict = new Dictionary<string, string>();
+                            for (var j = 0; j < storage.AvailableColumns.Length; ++j)
+                            {
+                                string subfieldName = storage.AvailableColumns[j];
+                                var subfield = row[subfieldName];
+
+                                if (subfield is Array a)
+                                {
+                                    for (var k = 0; k < a.Length; k++)
+                                    {
+                                        newDict.Add(subfieldName + "[" + k + "]", a.GetValue(k).ToString());
+                                    }
+                                }
+                                else
+                                {
+                                    newDict.Add(subfieldName, subfield.ToString());
+                                }
+                            }
+
+                            result.Add(newDict);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
